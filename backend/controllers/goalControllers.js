373919a089c9b144edff21f527c46101bbedd18a
aco <petras.aleksandar49@gmail.com>
2024-10-17 13:29:40 +0200

    create errorHandler and install express-async-handler package

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,23 +1,34 @@
+import asyncHandler from "express-async-handler"
+
+
 // @desc get goals
 // @route GET /api/goals
 // access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler( async (req, res) => {
   res.json({ mesage: "get goals", data: "all data" });
-};
+})
 // @desc set goal
 // @route POST /api/goals
 // access Private
-const setGoal = (req, res) =>
-  res.json({ mesage: "set goals", data: "all data" });
+const setGoal = asyncHandler( async (req, res) =>{
+    
+    if(!req.body.test) {
+         res.status(400)
+       throw new Error("Please add a text filed")
+    
+    };
+    
+  res.status(200).json({ mesage: "set goals", data: "all data" })
+})
 // @desc update goal
 // @route PUT /api/goals/:id
 // access Private
-const updateGoal = (req, res) =>
-  res.json({ mesage: `update goal ${req.params.id}`, data: "all data" });
+const updateGoal = asyncHandler( async (req, res) =>{
+  res.json({ mesage: `update goal ${req.params.id}`, data: "all data" })})
 // @desc delete goal
 // @route DELETE /api/goals/:id
 // access Private
-const deleteGoal = (req, res) =>
-  res.json({ mesage: `delete goal ${req.params.id}`, data: "all data" });
+const deleteGoal = asyncHandler( async (req, res) =>{
+  res.json({ mesage: `delete goal ${req.params.id}`, data: "all data" })})
 
 export { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 import goals from "./routes/goalRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use("/api/goals", goals);
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is ruuning on ${port}`));
